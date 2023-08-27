6d98bd6c8a9dd275697b60fc8fc349145edbd1d9
HoThinh468 <109681288+HoThinh468@users.noreply.github.com>
2023-08-27 23:12:39 +0700

    Setup dependencies for sqldelight and database related

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     //trick: for the same plugin versions in all sub-modules
     id("com.android.application").version("8.1.0").apply(false)
     id("com.android.library").version("8.1.0").apply(false)
+    id("com.squareup.sqldelight").version("1.5.5").apply(false)
+    id("com.google.dagger.hilt.android").version("2.44").apply(false)
     kotlin("android").version("1.8.21").apply(false)
     kotlin("multiplatform").version("1.8.21").apply(false)
 }
--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,10 +1,10 @@
 plugins {
     kotlin("multiplatform")
     id("com.android.library")
+    id("com.squareup.sqldelight")
 }
 
-@OptIn(org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi::class)
-kotlin {
+@OptIn(org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi::class) kotlin {
     targetHierarchy.default()
 
     android {
@@ -14,21 +14,24 @@ kotlin {
             }
         }
     }
-    
+
     listOf(
-        iosX64(),
-        iosArm64(),
-        iosSimulatorArm64()
+        iosX64(), iosArm64(), iosSimulatorArm64()
     ).forEach {
         it.binaries.framework {
             baseName = "shared"
         }
     }
 
+    val sqlDelightVersion = "1.5.5"
+    val dateTimeVersion = "0.4.0"
+
     sourceSets {
         val commonMain by getting {
             dependencies {
                 //put your multiplatform dependencies here
+                implementation("com.squareup.sqldelight:runtime:$sqlDelightVersion")
+                implementation("org.jetbrains.kotlinx:kotlinx-datetime:$dateTimeVersion")
             }
         }
         val commonTest by getting {
@@ -36,6 +39,23 @@ kotlin {
                 implementation(kotlin("test"))
             }
         }
+        val androidMain by getting {
+            dependencies {
+                implementation("com.squareup.sqldelight:android-driver:$sqlDelightVersion")
+            }
+        }
+        val iosMain by getting {
+            dependencies {
+                implementation("com.squareup.sqldelight:native-driver:$sqlDelightVersion")
+            }
+        }
+    }
+}
+
+sqldelight {
+    database("NoteDatabase") {
+        packageName="com.example.easynote.database"
+        sourceFolders = listOf("sqldelight")
     }
 }
 
